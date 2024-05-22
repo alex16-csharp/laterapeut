741f22f08aa2819654056a4f11476d09eb2d9782
alex16-csharp <akyalex16@yahoo.com>
2024-05-22 22:20:00 +0300

    first commit  + readmd + LICENSE cu licența MIT

---
+++ original index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Psihoterapie - psiholog Cristina Anghel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="/favicon.png">
+    
+    <link href="assets/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/style.css?v2" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Oswald&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    
+    <script src="assets/bootstrap.min.js"></script>
+</head>
+<body>
+<script type="text/javascript">
+$(function() {
+    $('#menu a').click(function (e) {
+        e.preventDefault();
+        console.log('asda');
+        $(this).tab('show');
+    });
+});
+</script>
+<div class="container" id="main">
+    <div class="main clearfix">
+    
+    <h4><i>&#8220; Nu poți învăța un om totul, îl poți ajuta doar să afle ceea ce este el cu adevărat &#8222;</i></h4>
+    <div class="text-right"><i>Galileo Galilei</i></div>
+    
+    <ul id="menu" class="nav nav-pills nav">
+        <li class="active"><a href="#home" >Home</a></li>
+        <li><a href="#psychotherapy" >Psihoterapia</a></li>
+        <li><a href="#faq">Psihologul răspunde</a></li>
+        <li><a href="#myths">Mituri despre depresie</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane active" id="home">
+        <p><b>
+            Psiholog Clinician cu drept de liberă practică Cristina Anghel <br />
+            Servicii psihoterapie și Hipnoza Ericksoniană <br /><br />
+            Psiholog colaborator în cadrul Asociației Rombet pentru programul Joc Responsabil <br />
+            Program de educare, prevenire și consiliere a persoanelor cu probleme de jocuri de noroc
+            </b>
+        </p>
+        <p>
+            Ai nevoie de psiholog dacă ai simțit că …
+        </p>
+        <ul>
+            <li>ești prea singur…</li>
+            <li>trăiești prea mult în trecut…</li>
+            <li>nu știi s-o iei de la capăt…</li>
+            <li>amâni la nesfârșit luarea unei decizii…</li>
+            <li>ți-e frică să îți exprimi punctul de vedere…</li>
+            <li>ți-e greu să spui "nu"…</li>
+        </ul>
+
+        <hr />
+        <ul>
+            <li>nu mai știi cine ești…</li>
+            <li>ți-e frică să nu-ți pierzi mințile…</li>
+            <li>nu vezi rostul zilei de mâine…</li>
+            <li>îți lipsește ceva, fără a putea spune ce…</li>
+            <li>ești nemulțumit de aspectul fizic…</li>
+            <li>ești neîmplinit profesional…</li>
+        </ul>
+        <hr />
+        <ul>
+            <li>nu te mai înțelegi cu familia, părinții, copiii, colegii…</li>
+            <li>ai probleme de relaționare în viața de cuplu…</li>
+            <li>nu mai ai dorință sexuală…</li>
+            <li>nu poți să-ți faci prieteni…</li>
+            <li>nu mai știi să te bucuri de viață…</li>
+        </ul>
+        <p>
+        … și mai ai speranța că poți fii ajutat, apelează la un cabinet psihologic 
+        pentru câteva ședințe de consiliere sau pentru psihoterapie. Este spațiul în 
+        care poti simți că cineva te ascultă fară să te judece, te acceptă așa cum 
+        ești și te va ajuta să poți să te ajuți singur.
+        </p>
+ 
+        </div>
+        <div class="tab-pane" id="psychotherapy">
+            Psihoterapia se desfășoară într-un spațiu sigur, confidențial, în care 
+            te simți acceptat și în care poti explora forțele conștiente și inconștiente 
+            care îți motivează comportamentele. Într-o psihoterapie poți deveni conștient 
+            de lumea ta interioară și vei dobândi o privire mai obiectivă asupra dificultăților tale.
+            <br />
+            Psihoterapia individuală se adresează celor care sunt într-un impas existențial sau 
+            celor care vor să se dezvolte personal, ajutandu-te să îți depășești dificultățile 
+            emoționale și relaționale, să faci schimbări care să-ți îmbunătățească viața.
+            <br />
+            Sedintele de psihoterapie sunt confidențiale și dureaza 50 de minute.
+            <br />
+            <br />
+            Psihoterapia poate să te ajute în situații precum:
+            <ul>
+                <li>perioade de schimbări și tranzitii</li>
+                <li>separare sau divorț</li>
+                <li>pierdere (doliu)</li>
+                <li>sentimente de neajutorare, pesimism</li>
+                <li>furie excesivă sau iritabilitate</li>
+                <li>stimă de sine scazută, devalorizare</li>
+                <li>abuz de substanțe (alcool, drog)</li>
+                <li>dependență jocuri de noroc</li>
+                <li>anxietate, atacuri de panică, fobii</li>
+                <li>depresie</li>
+                <li>retragere din viața socială</li>
+                <li>probleme de relaționare</li>
+                <li>dificultăți în luarea deciziilor</li>
+                <li>sentimente de vinovăție</li>
+                <li>experiența trăirii unor situații traumatice în trecut sau în prezent</li>
+                <li>tulburări alimentare, ale imaginii corporale</li>
+                <li>dificultăți legate de somn</li>
+            </ul>
+        </div>
+        <div class="tab-pane" id="faq">
+
+            <h3>Ce este depresia?</h3>
+            <p>
+                Depresia pe scurt poate fi descrisa ca o stare de tristete prelungita 
+                cu accese inexplicabile de plâns, irascibilitate, sentimente de inutilitate, 
+                de lipsa desperantă, de neajutorare, indiferenta fata de orice.
+            </p>
+            <p>
+                Sunt prezente sentimente de vina nejustificate, activitatile agreabile, munca,
+                prietenii care în trecut faceau placere acum nu mai trezesc niciun interes. 
+            </p>
+            <p>
+                Pot aparea diminuari ale capacitatii memoriei, concentrarii, luarii deciziilor, 
+                apar gânduri de moarte cu/fara tentative suicidare.<br />
+                In plan corporal apare lipsa poftei de mincare sau apetit exagerat, insomnia sau 
+                somnolenta excesiva, dureri corporale difuze, articulare musculare, 
+                crampe abdominale, diaree/constipatie, manifestari care nu au o 
+                cauzalitate medicala obiectiva.
+            </p>
+            <h3>Care sunt cauzele apariției depresiei?</h3>
+            <p>
+            Factori considerați ca fiind determinanți în apariția depresiei<br />
+            Durerea trăită în urma decesului unei persoane dragi, despărțirea de persoana 
+            iubită sau alte pierderi pot determina apariția depresiei. 
+            Situațiile stresante din viața noastră, cum ar fi un abuz, pierderea locului de muncă sau schimbarea bruscă a situației financiare pot de asemenea facilita apariția depresiei.<br />
+            Drogurile și alcoolul pot determina apariția unui dezechilibru biochimic la nivelul creierului. 
+            Acest lucru poate cauza un episod depresiv. Unele persoane depresive 
+            recurg la droguri sau alcool pentru "a uita" de necazuri dar nu fac decât să-și agraveze starea depresivă.
+            </p>
+            <h3>Ce pot face dacă am depresie?</h3>
+<p>
+            Daca avem semne de intrebare legate de prezenta unei tulburari depresive sau daca persoanele apropiate semnaleaza ca ceva este în neregula cu noi, atunci este oportun sa cerem ajutorul unui specialist (medic psihiatru sau psiholog cu formare în psihoterapie).
+</p>
+<p>
+            Prima vizită poate fi la medicul psihiatru care poate sa spuna daca o persoana sufera sau nu de depresie, care este intensitatea suferintei și ce tratament medicamentos este recomandat pentru afectiunea respectiva. Medicul va informa pacientul despre modul în care actioneaza medicamentele și despre efectele secundare ale acestora.
+</p>
+<p>
+            Fie ca pacientul se hotaraste sau nu sa urmeze un tratament medicamentos, el are optiunea de a urma o psihoterapie  care sa il ajute sa depaseasca problema cu care se confrunta.
+</p>
+            <h3>Hipnoza se aplica în depresie?</h3>
+<p>
+            Hipnoza folosita în tratarea depresiei,descopera cauza profunda a tulburarii, în timp ce ajuta persoana sa gaseasca strategii de copping eficiente. Deasemenea, ajuta persoanele depresive sa obtina o stare generala mai buna caracterizata de sentimente de fericire și inlatura sau diminueaza gandurile negative care acompanieaza depresia. Tratamentul hipnoterapeutic pentru aceasta tulburare utilizeaza o combinatie intre sugestii și imagerie pentru a aduce schimbari pozitive în procesul inconstient al persoanei depresive. Persoanele care folosesc hipnoza pentru depresie vor experimenta deseori un sentiment de eliberare și un sentiment puternic al detinerii controlului asupra gandurilor lor și asupra vietii lor în general.
+</p>
+            <h3>Ce este anxietatea sociala?Fobia sociala?</h3>
+            <p>
+                Vorbim de fobie socială (sau anxietate socială) atunci când o persoană se 
+                simte mereu nesigură, neliniștită, tensionată sau chiar îngrozită 
+                în anumite tipuri de situații sociale sau în majoritatea situațiilor sociale. 
+            </p>
+            <p>
+                Fobia socială este o formă a anxietății clinice care se întâlnește la 7-12% din oameni 
+                (fiind una dintre cele mai prevalente probleme de anxietate) și debutează 
+                de regulă în adolescență sau la începutul vârstei adulte. 
+            </p>
+            <p>
+                În primul rând trebuie să înțelegem că este firesc pentru cei mai mulți 
+                oameni ca în anumite situații sociale să se simtă tensionați sau emoționați. 
+                De exemplu, poate fi vorba de un interviu, un discurs în fața unui public, 
+                o întâlnire cu o persoană dragă, o discuție cu o persoană aflată într-o 
+                poziție de autoritate (ex. șeful nostru) sau cunoașterea unor persoane noi. 
+                <br />
+                În astfel de situații, ni se poate întâmpla adesea să ne bată inima mai tare, 
+                să transpirăm, să ne înroșim, să ne tremure vocea sau mâinile. 
+                <br />
+                Dar aceste lucruri de regulă se reduc pe măsură ce trece timpul și ne 
+                acomodăm cu situația în care ne aflăm și nu ne împiedică să facem ceea ce ne-am propus. 
+                <br />
+                Nu de puține ori ni se întâmplă să ne uităm înapoi la o astfel de 
+                situație și să ne zicem: "Am fost puțin emoționat, dar a mers bine! Mi-a plăcut!" 
+                sau "M-am cam bâlbâit, dar m-am descurcat eu cumva. N-a fost așa de rău."
+</p>
+        </div>
+        <div class="tab-pane" id="myths">
+
+<p>
+Exista nenumarate mituri legate de depresie, mituri aparute ca urmare a confundarii DEPRESIEI cu starea de TRISTETE sau STRES.
+</p>
+ 
+<p>
+    <b>MIT "Nu sunt depresiv am doar o pasa proasta. Imi trece mie…"</b>
+<br/>
+    REALITATE: Depresia este o boala care poate afecta serios toate aspectele vietii tale, incluzând relatiile cu alte persoane, performanta la locul de munca, cat  și posibilitatea de a te bucura de ceea ce iti place sa faci în timpul tau liber.
+<p>
+ 
+<p>
+    <b>MIT "Eu nu o sa fiu niciodata depresiv"</b>
+<br/>
+    REALITATE: Depresia nu alege, ea poate sa apara la oricare persoana, varstnica sau tanara, bogata sau saraca, cu un IQ ridicat sau scazut.
+</p>
+ 
+<p>
+    <b>MIT "O baie fierbinte, o vorba buna și un pahar de CEVA e tot ce am nevoie"</b>
+<br/>
+    REALITATE:  Depresia este o tulburare de dispozitie și nu o simpla "pasa proasta" care trece de la sine daca ne dorim suficient de mult! Depresia necesita tratament de specialitate și nu sfaturi și remedii "babesti" citite în reviste.
+</p>
+ 
+<p>
+    <b>MIT "Daca gandesc pozitiv o sa ma fac bine" ; "Trebuie sa nu ma mai gandesc la asta și imi trece"</b>
+<br/>
+    REALITATE: Depresia este o tulburare reala (o boala de organ) și tratabila. Nu este posibil sa vindeci depresia prin simpla putere a gandirii pozitive. Cu siguranta gandirea pozitiva te ajuta sa gestionezi mai bine diagnosticul insa nu te va face bine peste noapte. Sa iti impui sa gandeasti pozitiv pentru a scapa de tulburarile de dispozitie este ca și cum i-ai cere unui diabetic sa-si schimbe concentratia de zahar din sange doar gandinduse la asta. Tinand cont ca depresia inseamna pierderea sperantei, cere ajutorul unui specialist pentru a primi tratamentul adecvat și pentru a te simti mai bine!
+</p>
+ 
+<p>
+    <b>MIT "Sunt depresiv. Imi trebuie o vacanta sau o sesiune de shopping"</b>
+<br />
+    REALITATE: Atunci cand treceti printrun episod depresiv și alegeti ca solutie diverse metode de relaxare, acestea doar vor ameliora pentru scurt timp simptomele, insa depresia nu va trece.
+</p>
+  
+<p>
+    <b>MIT "Daca ma duc la psihiatru, lumea o sa ma creada nebun"</b>
+<br />
+    REALITATE: Sa cauti tratament este decizia cea mai inteligenta care necesita forta și intelegere a diagnosticului. Tulburarile tale de dispozitie nu sunt nici defecte, nici slabiciuni.  Sa soliciti tratament inseamna ca ai curaj sa gasesti o cale pentru a te simti mai bine. Apeleaza la psiholog pentru un plan terapeutic personalizat evolutiei afectiunii tale.
+</p>
+ 
+<p>
+<b>MIT "Daca iau anti-depresive devin o leguma"</b>
+<br/>
+REALITATE: Anti-depresivele sunt prescrise în functie de evolutia afectiunii și istoricul personal al fiecarui pacient. Exista mai multe tipuri de antidepresive, fiecare conceput pentru a se potrivi inclusiv cu stilul de viata al pacientului. Cu cat apelezi la sfatul unui psihiatru mai repede, cu atat vei sesiza o ameliorare mai rapida a afectiunii. Nu toate anti-depresivele amortesc simturile. Ele actioneaza tintit pe nevoile voastre terapeutice ajutand la depasirea episodului depresiv. Anti-depresivele de generatie noua au efecte secundare similare placebo (reactii adverse rar intalnite).
+</p>
+ 
+<p>
+<b>MIT "Antidepresivele imi altereaza personalitatea!"</b>
+<br/>
+REALITATE: Pastilele nu iti vor afecta capacitatea de judecata și nici un iti vor da senzatia de fals curaj, ci te vor ajuta în lupta contra depresiei daca le vei lua la recomandarea și sub supravegherea  unui medic specialist.
+</p>
+ 
+<p>
+<b>MIT "Depresia mea nu are leac!"</b>
+<br />
+ REALITATE: Daca tulburarile tale de dispozitie sunt corect diagnosticate și tratate, poti sa duci o viata normala și sanatoasa. Exista milioane de persoane care se confrunta cu aceasta problema în toata lumea. Statisticile arata ca o persoana din cinci sufera de depresie, asa ca nu esti singura persoana care trece prin asa ceva. Exista tratamente personalizate, trebuie doar sa mergi la medicul psihiatru și psiholog pentru un consult și ajutor de specialitate.
+</p>
+  
+<p>
+<b>MIT "Sunt tanar/batran este normal sa am astfel de tulburari depresive !"</b>
+<br />
+ REALITATE: Tulburarile depresive nu tin cont de varsta. Nu este o lregula care spune ca un tanar sau un batran trebuie sa sufere de depresie. Depresia are o multitudine de factori declansatori. Trebuie sa iei în serios schimbarile bruste de dispozitie. Studii recenta arata ca inca de cand sunt sugari copiii pot suferi de astfel de tulburari. La batranete riscurile de depresie iti vor creste . De aceea este foarte important sa mergi la psiholog pentru a fi tratat în functie de necesitatile și simptomele tale.
+</p>
+  
+<p>
+<b>MIT "Sunt doar stresat nu depresiv"</b>
+<br />
+ REALITATE: Stresul și depresia alearga prin aceleasi circuite ale creierului , din acest motiv, tensiunea severa, pe termen lung , iti poate declansa o depresie. La fel cum și intamplari neplacute precum abuzul sexual sau tulburari de stres post-traumatice te pot afecta și iti pot declansa o depresie.
+</p>
+  
+<p>
+<b>MIT "Sunt doar trist, sigur nu sufar de depresie!"</b>
+<br />
+REALITATE: Este normal sa simti durere cand treci prin momente dificile, cum ar fi o despartire, moartea unei fiinte dragi sau pierderea unei slujbe. Daca insa senzatia de tristete se prelungeste pe o perioada mare de timp și acesteia i se adauga și alte simptome – epuizare fizica, insomnie, lipsa poftei de mancare etc. atunci trebuie sa consulti un psiholog pentru a-ti indica ce trebuie sa faci.
+</p>
+ 
+<p>
+<b>MIT "Depresia imi afecteaza viata sexuala"</b>
+<br />
+REALITATE: Poti  sa ai o viata sexuala normala chiar daca iti tratezi depresia. Antidepresivele moderne iti permit acest lucru.
+</p>
+        </div>
+        <div class="tab-pane" id="contact">
+            <h3>Psiholog Cristina Anghel</h3>
+            <p>
+            Facultatea de Psihologie – Universitatea Titu Maiorescu Bucuresti
+            <br />
+            Master Psihologie clinica, Hipnoza și Interventii terpeutice de scurta durata - Universitatea Titu Maiorescu Bucuresti.
+            <br />
+            Cod personal Registrul Unic al psihologilor cu drept de libera practica 15285.
+            </p>
+            <h4> 0735.555.350 / 0744.832.473</h4>
+            <h4><a href='mailto&#58;p%73i&#104;ol&#111;&#37;&#54;&#55;&#46;angh&#101;l&#99;r&#105;stin&#97;&#64;yah%6F%6F&#46;r%&#54;F'>psiholog&#46;ang&#104;elc&#114;&#105;stina&#64;yah&#111;o&#46;ro</a></h4>
+            <h4><a href="http://www.laterapeut.ro/">http://www.laterapeut.ro</a></h4>
+           
+        </div>
+    </div>
+
+ 
+    </div>
+</div>
+    
+<div class="footer">
+    <div class="container clearfix">
+        &#169;
+    </div>
+</div>
+</body>
+</html>
---
+++ read.md
@@ -0,0 +1,29 @@
+# La Terapeut
+Access <a href="https://alex16-csharp.github.io/laterapeut/">link</a> 
+
+Original website www.laterapeut.ro/
+
+# Psihoterapie - Site web pentru suport psihologic
+
+Acest proiect reprezintă un website dedicat serviciilor de psihoterapie, oferind resurse și suport pentru cei care caută ajutor profesional în gestionarea problemelor emoționale și psihologice.
+
+
+## Despre acest proiect
+
+Acest website este creat pentru a oferi o platformă accesibilă și informativă pentru cei care caută ajutor psihologic. Oferim resurse valoroase, informații despre diferite tipuri de terapie și posibilitatea de a contacta psihologi autorizați.
+
+### Caracteristici ale website-ului:
+
+- **Informații detaliate despre tipurile de terapie.**
+- **Secțiune de întrebări frecvente pentru a răspunde la cele mai comune întrebări despre psihoterapie.**
+- **Profiluri de psihologi cu specializările și experiența lor.**
+- **Opțiuni de programare online pentru sesiuni de terapie.**
+- **Resurse și articole pentru îmbunătățirea sănătății mintale.**
+
+## Contribuții
+
+Dacă dorești să contribui la acest proiect, te rugăm să deschizi un pull request sau să ne contactezi direct. Suntem deschiși la sugestii și îmbunătățiri pentru a face acest site cât mai util pentru utilizatorii noștri.
+
+## Licență
+
+Acest proiect este licențiat sub Licența MIT. Vezi fișierul `LICENSE` pentru mai multe detalii.
