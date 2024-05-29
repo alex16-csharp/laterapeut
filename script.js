58ef4629cee9834cf6d0cc14ab7dcc956209d1ab
alex16-csharp <akyalex16@yahoo.com>
2024-05-29 19:57:18 +0300

    "Updated index.html, script.js, style.css, style.css.map, and style.scss files with changes to sidebar, main content, and styling."

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function myFunction() {
 
 // sidebar
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "280px";
+    document.getElementById("main").style.marginLeft = "280px";
 }
 
 function closeNav() {
