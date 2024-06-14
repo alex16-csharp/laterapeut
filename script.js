91fa8f1955c0db75dc06a23cfa7e2dc62fee8a40
alex16-csharp <akyalex16@yahoo.com>
2024-06-14 19:57:11 +0300

    Improved sidebar navigation: added responsive design, reorganized links, and enhanced accessibility for better user experience

--- script.js
+++ script.js
@@ -25,13 +25,11 @@ window.addEventListener('scroll', function () {
 
 // sidebar
 function openNav() {
-    document.getElementById("mySidebar").style.width = "280px";
-    document.getElementById("main").style.marginLeft = "280px";
+    document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("myNav").style.width = "0%";
 }
 
 // Get the body element
