a43a7a38e7076415aacf74414f2b86151b78d7df
alex16-csharp <akyalex16@yahoo.com>
2024-07-12 12:15:55 +0300

    "Updated index.html, script.js, style.css, and style.css.map files; added animejs library; modified CSS styles and animations"

--- script.js
+++ script.js
@@ -86,3 +86,38 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.ml5 .line',
+        opacity: [0.5, 1],
+        scaleX: [0, 1],
+        easing: "easeInOutExpo",
+        duration: 1000
+    }).add({
+        targets: '.ml5 .line',
+        duration: 1000,
+        easing: "easeOutExpo",
+        translateY: (el, i) => (-1.125 + 1.125 * 2 * i) + "em"
+    }).add({
+        targets: '.ml5 .ampersand',
+        opacity: [0, 1],
+        scaleY: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 1600,
+        offset: '-=600'
+    }).add({
+        targets: '.ml5 .letters-left',
+        opacity: [0, 1],
+        translateX: ["0.5em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=300'
+    }).add({
+        targets: '.ml5 .letters-right',
+        opacity: [0, 1],
+        translateX: ["-0.5em", 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=600'
+    })
